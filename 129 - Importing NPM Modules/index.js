4f2b82813989b6d76e1d80286d34b06c26ee5f52
Md Mahmudul Hassan <mrepte700@gmail.com>
2021-05-16 23:27:30 +0600

    Add files via upload

---
+++ 129 - Importing NPM Modules/index.js
@@ -0,0 +1,8 @@
+const validator = require('validator');
+
+console.log('started..')
+// console.log(validator.isEmail('mrepte700@gmail.com1'));
+// console.log(validator.isJSON(JSON.stringify({name: 'sakib', age: 20})));
+// console.log(validator.isPort('65536'));
+console.log(validator.isURL('https://kutumbari.tk'));
+//0 to 65535 is port number range
